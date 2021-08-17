7d21e162bd783e8adf42a354eefa853cfcee9c79
fernandasnunes <fernandadasilvanunes0@gmail.com>
2021-08-17 13:18:02 -0300

    organizando pastas

--- exceptions/java-pilha/src/FluxoComTratamento.java
+++ exceptions/java-pilha/src/FluxoComTratamento.java
@@ -4,7 +4,7 @@ public class FluxoComTratamento {
 		System.out.println("Ini do main");
 		try{
 		    metodo1();
-		} catch(ArithmeticException | NullPointerException ex) {
+		} catch(ArithmeticException | MinhaException| NullPointerException ex) {
 		    String msg = ex.getMessage();
 		    System.out.println("ArithmeticException " + msg);
 		    ex.printStackTrace();
@@ -12,14 +12,15 @@ public class FluxoComTratamento {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaException {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaException {
 	    System.out.println("Ini do metodo2");
-	    throw new ArithmeticException("Deu erro");
+	    //lançando minha exception
+	    throw new MinhaException("Algo deu errado");
 	}
 }
